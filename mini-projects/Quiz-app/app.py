7048ee217214ff54841314354e4bedbf58cbe158
Web-Affan-Farooq <affanamir903@gmail.com>
2025-04-12 22:28:13 +0500

    added todolist project with basic functionalities

--- mini-projects/Quiz-app/app.py
+++ mini-projects/Quiz-app/app.py
@@ -3,66 +3,83 @@ import streamlit as st
 import json
 import random
 import matplotlib.pyplot as plt
+from typing import Dict,List
 
 def load_questions():
     with open("./data/questions.json", "r") as f:
         questions = json.load(f)
         return questions
-    
-st.title("Quiz application ")
-st.subheader("Let's check how sharp is your python programming skills are ")
 
-questions = load_questions()[0:11]
+questions = load_questions()[0:11] # Select first 10 questions 
 
+# __Basic structure 
 #   {
 #     "question": "Which of the following functions converts a string to an integer in Python?",
 #     "options": ["str()", "int()", "float()", "chr()"],
 #     "correct_answer": "int()"
 #   },
 
+# ____Sessionn state working correctly
 if("count" not in st.session_state):
     st.session_state.count = 0  
+if("quiz_status" not in st.session_state):
+    st.session_state.quiz_status = False
 if("selected_options" not in st.session_state):
     st.session_state.selected_options = [] 
 if("correct_options" not in st.session_state):
     st.session_state.correct_options = []
 if("score" not in st.session_state):
     st.session_state.score = 0
-    
-if st.button("Start quiz "):
 
+# state: Dict[int,bool,List[str],List[str],int]= st.session_state
+
+def quiz_start():
     for question in questions:
         st.write(f"{st.session_state.count}. "+question["question"])
         options = question["options"]
-        selected_option = st.radio("",options=options,index=None, key=st.session_state.count)
-        st.session_state.selected_options.append(selected_option)
-        st.session_state.correct_options.append(question["correct_answer"])
-        st.session_state.count += 1
-        st.write(" ")
-        st.write(" ")
-        st.write(" ")
+        selected_option = st.radio("",options=options, index=None, key=random.randint(0,1000))
+        if selected_option is not None:
+            # st.write("Selected option ",selected_option)
+            st.session_state.selected_options.append(selected_option)
+            # st.write("All selected options :",st.session_state.selected_options)
+            st.session_state.correct_options.append(question["correct_answer"])
+            st.session_state.count += 1
+            st.write(" ")
+            st.write(" ")
+            st.write(" ")
+        
+def change_state():
+    st.session_state.quiz_status = not st.session_state.quiz_status
+if(st.session_state.quiz_status):
+    quiz_start()
+else :
+    st.title("Quiz application ")
+    st.subheader("Let's check how sharp is your python programming skills are ")
+    st.write(st.session_state)
+    st.button("Start quiz ", on_click=change_state)
 
-    if st.button("Submit"):
-        if(None not in st.session_state.selected_options):
-            st.write("Selected options :",st.session_state.selected_options)
-            st.write("Correct answers : ",st.session_state.correct_options)
-            for i in range(11):
-                if (st.session_state.selected_options[i] == st.session_state.correct_options[i]): 
-                    score += 1
-                else : 
-                    pass
-            st.subheader(f"Score : {len(st.session_state.correct_options)-1} / {st.session_state.score}")
+
+#     if st.button("Submit"):
+#         if(None not in st.session_state.selected_options):
+#             st.write("Selected options :",st.session_state.selected_options)
+#             st.write("Correct answers : ",st.session_state.correct_options)
+#             for i in range(11):
+#                 if (st.session_state.selected_options[i] == st.session_state.correct_options[i]): 
+#                     score += 1
+#                 else : 
+#                     pass
+#             st.subheader(f"Score : {len(st.session_state.correct_options)-1} / {st.session_state.score}")
     
-            incorrect_answers_count = len(st.session_state.correct_options) - st.session_state.score
-            labels = ['Correct Answers', 'Incorrect Answers']
-            scores = [st.session_state.score, incorrect_answers_count]
-            fig, ax= plt.subplots()
-            ax.pie(scores, labels=labels, autopct='%1.1f%%', startangle=90, colors=['#1364e8', '#ff6666'], textprops={"color":"white"})    
+#             incorrect_answers_count = len(st.session_state.correct_options) - st.session_state.score
+#             labels = ['Correct Answers', 'Incorrect Answers']
+#             scores = [st.session_state.score, incorrect_answers_count]
+#             fig, ax= plt.subplots()
+#             ax.pie(scores, labels=labels, autopct='%1.1f%%', startangle=90, colors=['#1364e8', '#ff6666'], textprops={"color":"white"})    
     
-            ax.axis('equal')  # Equal aspect ratio ensures that the pie is drawn as a circle
+#             ax.axis('equal')  # Equal aspect ratio ensures that the pie is drawn as a circle
     
-            fig.patch.set_facecolor("#000000")
-# Display the pie chart in Streamlit
-            st.pyplot(fig)
-        else : 
-            st.toast("Please attemp all questions ")
+#             fig.patch.set_facecolor("#000000")
+# # Display the pie chart in Streamlit
+#             st.pyplot(fig)
+#         else : 
+#             st.toast("Please attemp all questions ")
---
+++ mini-projects/todo-list-app/app.py
@@ -0,0 +1,101 @@
+import streamlit as st
+from streamlit_autorefresh import st_autorefresh
+from datetime import date
+
+if("Todos" not in st.session_state 
+   and "add_field_status" not in st.session_state
+   and "edit_field_status" not in st.session_state):
+    st.session_state.Todos = []
+    st.session_state.add_field_status = False
+    st.session_state.edit_field_status = False
+    
+# ____For pushing todos in list present in session state
+def create_todo(todo):
+    st.session_state.Todos.append(todo)
+    st.toast("Todo added successfully")
+    st.session_state.add_field_status = False # status not toogling 
+    st.session_state.edit_field_status = False # status not toogling 
+    st.write("Todos pushed : ",todo)
+    st.write("Current session state : ", st.session_state)
+    
+def show_fields():
+    title = st.text_input("Enter title")
+    description = st.text_area("Enter description")
+    due_date = st.date_input("Enter due date")
+    if(title != "" and description != ""):
+        data = {
+            "title":title,
+            "description":description,
+            "due-date":due_date,
+            "done":False,
+            "created_at":date.today()
+            }
+        st.write("Title : ",title)
+        st.write("Description : ",description)
+        st.write("Duedate : ",due_date)
+        if st.button("Create todo"):
+                return create_todo(data)
+        # print(f"Today is {date.today()}")
+        # print("title : ",title)
+        # print("description : ",description)
+        # print("duedate : ", due_date)
+
+st.title("Todolist application")
+create_todo_tab , edit_todo_tab, list_tab = st.tabs(["Create todos", "Edit todos ", "List"])
+with create_todo_tab:
+    st.session_state.add_field_status = True
+    show_fields()
+    
+with edit_todo_tab:
+    options = [todo["title"] for todo in st.session_state.Todos]
+    options.insert(0, "No option selected")
+    selected_todo_title = st.selectbox(label="Select todo ", options=options)
+    selected_todo = {}
+    if(selected_todo_title != options[0]):        
+        for todo in st.session_state.Todos:
+            if(todo["title"] == selected_todo_title):
+                selected_todo = todo
+            else : pass
+        new_title = st.text_input("Todo title ", value=selected_todo["title"])
+        new_description = st.text_input("Todo description ", value=selected_todo["description"])
+        new_due_date = st.date_input("Todo title ", value=selected_todo["due-date"])
+        
+        todo_index = options.index(selected_todo_title) - 1 # because we have added a default value "no option sleected in the list "
+        
+        st.write("index : ", todo_index)
+        if st.button("Save"):
+            st.session_state.Todos[todo_index]["title"] = new_title 
+            st.session_state.Todos[todo_index]["description"] = new_description
+            st.session_state.Todos[todo_index]["due-date"] = new_due_date 
+            st.toast("Saved ")
+            selected_todo_title = "No option selected"
+            st_autorefresh(interval=100, limit=1, key="refresh")
+    st.write(selected_todo)
+
+with list_tab:
+    def mark_as_done(todo_title, status):
+        for i, todo in enumerate(st.session_state.Todos):
+            if todo["title"] == todo_title:
+                st.session_state.Todos[i]["done"] = status
+                st.toast("Status updated")
+                st.rerun()
+
+    if st.session_state.Todos:
+        for todo in st.session_state.Todos:
+            st.write("#### Title:", todo["title"])
+            st.write("Description:", todo["description"])
+            st.write("Due date:", todo["due-date"])
+            st.write("Created at:", todo["created_at"])
+
+            if todo["done"]:
+                st.success("✅ Completed")
+                if st.button("Mark as Remaining", key=f"remaining_button_{todo['title']}"):
+                    mark_as_done(todo["title"], False)
+            else:
+                st.warning("⏳ Remaining")
+                if st.button("Mark as Done", key=f"done_button_{todo['title']}"):
+                    mark_as_done(todo["title"], True)
+
+            st.divider()
+    else:
+        st.info("No todos found...")
